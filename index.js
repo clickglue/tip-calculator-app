a62cd4997a05f3257a5ec7a2de6f68b4e4ada84a
clickglue <clickglue@telenet.be>
2024-10-31 17:53:57 +0100

    Finetuning and readme.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let bill = 0;
-let tip = 0;
+let bill = '';
+let tip = '';
 let people = 1;
 let tipTotal = 0;
 let total = 0;
@@ -18,11 +18,16 @@ function calc() {
   document.getElementById("total-result").innerText = twoDecimals(
     total / people
   );
+}
+
+function updateScreen() {
+
   document.getElementById("input-people").value = people;
   document.getElementById("input-bill").value = twoDecimals(bill);
 }
 
 function setTip(e) {
+  e.placeholder = "";
   document
     .querySelectorAll(".input .tip .input-container button")
     .forEach((btn) => btn.classList.remove("active"));
@@ -32,25 +37,45 @@ function setTip(e) {
 }
 
 function setBill(e) {
+  e.classList.add("active");
   bill = Number(e.value);
   calc();
 }
 
 function setPeople(e) {
+  e.classList.add("active");
   people = Number(e.value);
-  if (people <= 0) people = 1;
+  if (people <= 0) {
+    showWarning(true);
+    people = 1;
+  } else {
+    showWarning(false)
+  }
   calc();
 }
 
+function showWarning(check){
+  if(check){
+    document.getElementById('warning').style.display='block'
+
+  } else {
+    document.getElementById('warning').style.display='none'
+  }
+}
+
 function reset() {
-  console.log('reset')
-  bill = 0;
+  bill = null;
   tip = 0;
-  people = 1;
+  people = null;
   tipTotal = 0;
   total = 0;
   document
-  .querySelectorAll(".input .tip .input-container button")
-  .forEach((btn) => btn.classList.remove("active"));
-  calc();
+    .querySelectorAll(".input .tip .input-container button")
+    .forEach((btn) => btn.classList.remove("active"));
+  document
+    .getElementById("input-bill", "input-tip", "input-people")
+    .classList.remove("active");
+  document.getElementById("input-tip").placeholder = "Custom";
+  document.getElementById("input-tip").value = "";
+  updateScreen();
 }
